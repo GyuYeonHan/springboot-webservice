92d98d9a4069ff669b915620bc445ef1ff5a1b50
gyuyeon <gks951020@gmail.com>
2021-06-06 11:07:22 +0900

    Paint 추가 #4

--- src/main/java/com/gyuyeon/springbook/web/paintController.java
+++ src/main/java/com/gyuyeon/springbook/web/paintController.java
@@ -2,6 +2,7 @@ package com.gyuyeon.springbook.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class paintController {
@@ -9,5 +10,11 @@ public class paintController {
     @GetMapping("/paint")
     public String paint() {
         return "/paint/index";
+    }
+
+    @ResponseBody
+    @GetMapping("/paint1")
+    public String paint1() {
+        return "paint";
     }
 }
