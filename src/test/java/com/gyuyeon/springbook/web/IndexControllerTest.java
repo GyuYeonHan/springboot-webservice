d783db2fb3f250baf1c334b8692ecf5e3af12663
gyuyeon <gks951020@gmail.com>
2021-06-28 21:43:50 +0900

    실패 테스트 수정

--- src/test/java/com/gyuyeon/springbook/web/IndexControllerTest.java
+++ src/test/java/com/gyuyeon/springbook/web/IndexControllerTest.java
@@ -16,7 +16,7 @@ class IndexControllerTest {
     @Test
     void 메인페이지_로딩() {
         //when
-        String body = this.restTemplate.getForObject("/", String.class);
+        String body = this.restTemplate.getForObject("/posts", String.class);
 
         //then
         assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
